8d8765ee9033c77e804ce8e5c72f7c657fe35d98
Tomasz Habiger <tomasz.habiger@gmail.com>
2026-06-23 19:13:47 +0200

    fix: Collapse SpaceStateStore 7 @Published properties into single state struct [A4]

--- StayHere/Core/Space/SpaceStateStore.swift
+++ StayHere/Core/Space/SpaceStateStore.swift
@@ -1,14 +1,46 @@
 import Foundation
 import Combine
 
+public struct SpaceState: Equatable {
+    public var spaces: [SpaceIdentity] = []
+    public var activeSpaceID: Int?
+    public var labels: [Int: SpaceLabel] = [:]
+    public var displayOrder: [Int] = []
+    public var usesCustomDisplayOrder: Bool = false
+    public var desktopNumberBySpaceID: [Int: Int] = [:]
+    public var nativeOrderByDisplay: [String: [Int]] = [:]
+
+    public init() {}
+
+    public init(
+        spaces: [SpaceIdentity],
+        activeSpaceID: Int?,
+        labels: [Int: SpaceLabel],
+        displayOrder: [Int],
+        usesCustomDisplayOrder: Bool,
+        desktopNumberBySpaceID: [Int: Int],
+        nativeOrderByDisplay: [String: [Int]]
+    ) {
+        self.spaces = spaces
+        self.activeSpaceID = activeSpaceID
+        self.labels = labels
+        self.displayOrder = displayOrder
+        self.usesCustomDisplayOrder = usesCustomDisplayOrder
+        self.desktopNumberBySpaceID = desktopNumberBySpaceID
+        self.nativeOrderByDisplay = nativeOrderByDisplay
+    }
+}
+
 public final class SpaceStateStore: ObservableObject {
-    @Published public private(set) var spaces: [SpaceIdentity] = []
-    @Published public private(set) var activeSpaceID: Int?
-    @Published public private(set) var labels: [Int: SpaceLabel] = [:]
-    @Published public private(set) var displayOrder: [Int] = []
-    @Published public private(set) var usesCustomDisplayOrder: Bool = false
-    @Published public private(set) var desktopNumberBySpaceID: [Int: Int] = [:]
-    @Published public private(set) var nativeOrderByDisplay: [String: [Int]] = [:]
+    @Published public private(set) var state = SpaceState()
+
+    public var spaces: [SpaceIdentity] { state.spaces }
+    public var activeSpaceID: Int? { state.activeSpaceID }
+    public var labels: [Int: SpaceLabel] { state.labels }
+    public var displayOrder: [Int] { state.displayOrder }
+    public var usesCustomDisplayOrder: Bool { state.usesCustomDisplayOrder }
+    public var desktopNumberBySpaceID: [Int: Int] { state.desktopNumberBySpaceID }
+    public var nativeOrderByDisplay: [String: [Int]] { state.nativeOrderByDisplay }
 
     public init() {}
 
@@ -17,16 +49,27 @@ public final class SpaceStateStore: ObservableObject {
         displayOrder: [Int],
         usesCustomDisplayOrder: Bool
     ) {
-        self.labels = labels
-        self.displayOrder = displayOrder
-        self.usesCustomDisplayOrder = usesCustomDisplayOrder
+        state = SpaceState(
+            spaces: state.spaces,
+            activeSpaceID: state.activeSpaceID,
+            labels: labels,
+            displayOrder: displayOrder,
+            usesCustomDisplayOrder: usesCustomDisplayOrder,
+            desktopNumberBySpaceID: state.desktopNumberBySpaceID,
+            nativeOrderByDisplay: state.nativeOrderByDisplay
+        )
     }
 
     public func applyDerivedState(_ derivedState: SpaceRegistryDerivedState) {
-        spaces = derivedState.spaces
-        activeSpaceID = derivedState.activeSpaceID
-        nativeOrderByDisplay = derivedState.nativeOrderByDisplay
-        desktopNumberBySpaceID = derivedState.desktopNumberBySpaceID
+        state = SpaceState(
+            spaces: derivedState.spaces,
+            activeSpaceID: derivedState.activeSpaceID,
+            labels: state.labels,
+            displayOrder: state.displayOrder,
+            usesCustomDisplayOrder: state.usesCustomDisplayOrder,
+            desktopNumberBySpaceID: derivedState.desktopNumberBySpaceID,
+            nativeOrderByDisplay: derivedState.nativeOrderByDisplay
+        )
     }
 
     public func currentSwitchSnapshot() -> SpaceSwitchSnapshot {
